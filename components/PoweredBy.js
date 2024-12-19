245e6c926d9b1ce2f2cd23fc75e466ba08553c42
supervip-on <jianfengmuc@gmail.com>
2024-12-19 19:05:56 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,13 +8,11 @@ export default function PoweredBy(props) {
   return (
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
-      <span>Powered by</span>
+      <span></span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href=''
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
       </a>
-      .
     </div>
   )
 }
