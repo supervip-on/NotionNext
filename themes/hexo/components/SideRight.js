379bf1e83a403437447701fccbb0efff245ce150
supervip-on <jianfengmuc@gmail.com>
2025-10-12 08:29:27 +0800

    Update SideRight.js

--- themes/hexo/components/SideRight.js
+++ themes/hexo/components/SideRight.js
@@ -1,4 +1,4 @@
-import Live2D from '@/components/Live2D'
+//import Live2D from '@/components/Live2D'
 import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
 import dynamic from 'next/dynamic'
@@ -100,7 +100,7 @@ export default function SideRight(props) {
 
         {rightAreaSlot}
         <FaceBookPage />
-        <Live2D />
+        //<Live2D />
       </div>
     </div>
   )
