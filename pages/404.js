fee3da17bd3ff6503d4712a38873d22ca3650420
supervip-on <jianfengmuc@gmail.com>
2025-10-12 10:13:20 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -3,16 +3,6 @@ import { siteConfig } from '@/lib/config'
 import { getGlobalData } from '@/lib/db/getSiteData'
 //import { DynamicLayout } from '@/themes/theme'
 import { Layout404 } from '@/themes/hexo'
-/**
- * 404
- * @param {*} props
- * @returns
- */
-/* const NoFound = props => {
-   const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
-   return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
-}
-*/ 
 
 const NoFound = props => {
    return <Layout404 {...props} />
