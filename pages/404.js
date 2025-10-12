fb206affee8cdffb2dd1e46b021282a5af88dd75
supervip-on <jianfengmuc@gmail.com>
2025-10-12 09:36:32 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,20 @@
 import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
 import { getGlobalData } from '@/lib/db/getSiteData'
-import { DynamicLayout } from '@/themes/theme'
-
+//import { DynamicLayout } from '@/themes/theme'
+import { Layout404 } from '@/themes/hexo'
 /**
  * 404
  * @param {*} props
  * @returns
  */
+// const NoFound = props => {
+//   const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
+//   return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
+// }
+
 const NoFound = props => {
-  const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
-  return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
+   return <Layout404 {...props} />
 }
 
 export async function getStaticProps(req) {
